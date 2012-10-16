aee40f5c996aebb59202ff248582568e0b97f43e
Michael Thorpe <mt2309@ic.ac.uk>
2012-10-16 15:51:25 +0100

    Added some complete horrors:
    Finding an object in an array is done using exceptions, several methods.

--- src/com/github/mt2309/horrors/MyClassGettersSettersConstructorArrayGetterSetterConstructorWithMyBehaviour.java
+++ src/com/github/mt2309/horrors/MyClassGettersSettersConstructorArrayGetterSetterConstructorWithMyBehaviour.java
@@ -29,6 +29,45 @@ public class MyClassGettersSettersConstructorArrayGetterSetterConstructorWithMyB
         return null;
     }
 
+    public Object locateValueByEquality(Object o) {
+        try {
+            findObjectByObject(o);
+        }
+        catch (ObjectFoundException objectFoundException) {
+            return (objectFoundException.object == null) ? objectFoundException.object : null;
+        }
+
+        return null;
+    }
+
+    private Object findObjectByObject(Object o) throws ObjectFoundException {
+        return findInterestingObjectFromArrayUsingEquality(o);
+    }
+
+    private Object findInterestingObjectFromArrayUsingEquality(Object o) throws ObjectFoundException {
+        for(double i = 0; i <= Double.MAX_VALUE; i++) {
+            this.temporaryValue = this.array[new Double(i).intValue()];
+            if (this.temporaryValue == o) {
+                throw new ObjectFoundException(this.temporaryValue);
+            }
+        }
+        return null;
+    }
+
+    private class ObjectFoundException extends Exception {
+        Object object;
+
+        public ObjectFoundException(Object o) {
+            super();
+            this.object = o;
+        }
+
+        public ObjectFoundException() {
+            super();
+            this.object = null;
+        }
+    }
+
     @Override
     public boolean setnThelem(float n) {
         return false;  //To change body of implemented methods use File | Settings | File Templates.
